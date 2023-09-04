4dc7c78b8bfac2835e3a871fb6a156d05e84c12b
KeleD2000 <k.dominik2000@gmail.com>
2023-09-04 15:21:16 +0200

    header, textc done

--- src/app/videotar/vidik/vidik.component.ts
+++ src/app/videotar/vidik/vidik.component.ts
@@ -12,6 +12,8 @@ import { HtmlconvertService } from 'src/app/services/htmlconvert.service';
 export class VidikComponent {
   content: any[] = [];
   baseUrl: string = "https://baba.jrdatashu.win";
+  isTextCondensed: boolean = false;
+
   constructor(private fooldalService: FooldalService, private htmlconvertService: HtmlconvertService){}
 
   extractVideoId(url: string): string | null {
@@ -31,8 +33,8 @@ export class VidikComponent {
               this.fooldalService.getFooldal(v.id).subscribe((page) =>{
                 for(const [key, value] of Object.entries(page)){
                   for(var k in value.field_paragraphs){
-                    console.log(value.field_paragraphs[k]);
-                    const obj = {content: "" as SafeHtml, img_url: "", img_layout: "", youtube_video: "" };
+                    console.log(value.field_paragraphs);
+                    const obj = {content: "" as SafeHtml, img_url: "", img_layout: "", youtube_video: "", text_condensed: "" as SafeHtml };
                     if(value.field_paragraphs[k].type === 'paragraph--image_full'){
                       obj.img_url = this.baseUrl + value.field_paragraphs[k].field_image_full.field_media_image.uri.url;
                     }else if(value.field_paragraphs[k].type === 'paragraph--image_text_blue'){
@@ -47,6 +49,14 @@ export class VidikComponent {
                     }else if(value.field_paragraphs[k].type === 'paragraph--youtube_video'){
                       const videoId = this.extractVideoId(value.field_paragraphs[k].field_youtube_video.field_media_oembed_video);
                       obj.youtube_video = "https://www.youtube.com/embed/"+ videoId
+                    }else if(value.field_paragraphs[k].type === 'paragraph--text_condensed'){
+                      const paragraph_condensed = this.htmlconvertService.convertToHtml(value.field_paragraphs[k].field_content.value);
+                      obj.text_condensed = paragraph_condensed;
+                      if(obj.text_condensed){
+                        this.isTextCondensed = true;
+                      }else{
+                        this.isTextCondensed = false;
+                      }
                     }
                     this.content.push(obj);
                   }
