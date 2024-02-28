5d58e9a83c8c6435fb334a87318310eee7c1e9ec
KeleD2000 <k.dominik2000@gmail.com>
2024-02-28 11:23:18 +0100

    updatelt

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElofizetesComponent } from './elofizetes/elofizetes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ElofizetesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
